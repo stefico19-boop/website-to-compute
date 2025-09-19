f53d7fbba48e88f653331976fa19cf8208471615
stefico19-boop <stefico19@gmail.com>
2025-09-19 16:43:12 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+<script type="module">
+  // הדביקי כאן את הערכים מהקונסולה של Firebase
+  export const firebaseConfig = {
+    apiKey: "PASTE",
+    authDomain: "PASTE.firebaseapp.com",
+    projectId: "PASTE",
+    storageBucket: "PASTE.appspot.com",
+    messagingSenderId: "PASTE",
+    appId: "PASTE"
+  };
+</script>
